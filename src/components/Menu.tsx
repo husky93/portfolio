56bc2daa2930b858431a7e71d5387e4f04bf594f
Maciej Sroka <sroka.maciej93@gmail.com>
2022-10-07 15:51:40 +0200

    Animate links on menu toggle

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -5,9 +5,10 @@ const MenuLink = lazy(() => import('./MenuLink'));
 
 interface MenuProps {
   active: boolean;
+  handleLinkClick: React.MouseEventHandler<HTMLAnchorElement>;
 }
 
-const Menu: React.FC<MenuProps> = ({ active }) => {
+const Menu: React.FC<MenuProps> = ({ active, handleLinkClick }) => {
   return (
     <nav
       aria-hidden={active ? 'false' : 'true'}
@@ -15,10 +16,18 @@ const Menu: React.FC<MenuProps> = ({ active }) => {
       className={`${styles.navigation} ${active ? styles.active : ''}`}
     >
       <ul className={styles.list}>
-        <MenuLink to="#hero">Home</MenuLink>
-        <MenuLink to="#hero">About</MenuLink>
-        <MenuLink to="#hero">Projects</MenuLink>
-        <MenuLink to="#hero">Contact</MenuLink>
+        <MenuLink to="#hero" handleClick={handleLinkClick}>
+          Home
+        </MenuLink>
+        <MenuLink to="#about" handleClick={handleLinkClick}>
+          About
+        </MenuLink>
+        <MenuLink to="#projects" handleClick={handleLinkClick}>
+          Projects
+        </MenuLink>
+        <MenuLink to="#contact" handleClick={handleLinkClick}>
+          Contact
+        </MenuLink>
       </ul>
     </nav>
   );

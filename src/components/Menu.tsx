73cf471eb58dd7b210fc053ec57d4a65bbee1a5b
Maciej Sroka <sroka.maciej93@gmail.com>
2022-10-07 13:11:44 +0200

    Initialize Menu component template files

---
+++ src/components/Menu.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import styles from '../assets/styles/Menu.module.css';
+
+interface MenuProps {}
+
+const Menu: React.FC<MenuProps> = ({}) => {
+  return <></>;
+};
+
+export default Menu;

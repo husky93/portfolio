e3740c4b7ac0dddfec7c34e33f025ecb2ae75a0a
Maciej Sroka <sroka.maciej93@gmail.com>
2022-10-07 17:42:04 +0200

    Setup About section initial template files

---
+++ src/components/About.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import styles from '../assets/styles/About.module.css';
+
+interface AboutProps {}
+
+const About: React.FC<AboutProps> = ({}) => {
+  return <section id="#about"></section>;
+};
+
+export default About;

c5308a98bc5be5703d6849aa365034d901cb9fe8
Maciej Sroka <sroka.maciej93@gmail.com>
2022-10-07 11:40:10 +0200

    Move toggle menu logic to App component

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,20 @@ import Loading from './components/Loading';
 import styles from './assets/styles/Home.module.css';
 
 const Hero = lazy(() => import('./components/Hero'));
+const Hamburger = lazy(() => import('./components/Hamburger'));
 
 function App() {
+  const [menuActive, setMenuActive] = useState(false);
+
+  const toggleMenu: React.MouseEventHandler<HTMLButtonElement> = (): void => {
+    setMenuActive((prevState) => !prevState);
+  };
+
   return (
-    <Suspense fallback={<div>Loading...</div>}>
+    <Suspense fallback={<Loading />}>
       <div className={styles.container}>
         <main className={styles.main}>
-          <Loading />
+          <Hamburger active={menuActive} handleClick={toggleMenu} />
           <Hero />
         </main>
       </div>

610a43cffdc7576c17305b7df334e780440336e9
Maciej Sroka <sroka.maciej93@gmail.com>
2022-10-08 14:09:25 +0200

    Add useParallax hook

--- src/app/hooks.ts
+++ src/app/hooks.ts
@@ -1,7 +1,7 @@
-import React, { useRef, useState, useEffect } from 'react';
+import { useRef, useState, useEffect } from 'react';
 import type { RefObject } from 'react';
 
-export const useObserver = (threshold: number) => {
+export const useObserver = (threshold: number): ObserverObject => {
   const containerRef: RefObject<HTMLElement> = useRef(null);
   const [isVisible, setIsVisible] = useState(false);
 
@@ -26,4 +26,20 @@ export const useObserver = (threshold: number) => {
   }, [containerRef, options]);
 
   return { containerRef, isVisible };
+};
+
+export const useParallax = (): ParallaxObject => {
+  const [offset, setOffset] = useState(0);
+  const [windowHeight, setWindowHeight] = useState(0);
+
+  useEffect(() => {
+    const onScroll = () => setOffset(window.scrollY);
+    // clean up code
+    window.removeEventListener('scroll', onScroll);
+    window.addEventListener('scroll', onScroll, { passive: true });
+    setWindowHeight(window.innerHeight);
+    return () => window.removeEventListener('scroll', onScroll);
+  }, []);
+
+  return { offset, windowHeight };
 };
--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,11 @@
 /// <reference types="vite/client" />
+
+interface ObserverObject {
+  containerRef: RefObject<HTMLElement>;
+  isVisible: boolean;
+}
+
+interface ParallaxObject {
+  offset: number;
+  windowHeight: number;
+}

e95250ecf2b263baa5b3e7c8aac6c68cdebeccf3
Maciej Sroka <sroka.maciej93@gmail.com>
2022-10-08 14:52:08 +0200

    Create Skills section template files

---
+++ src/components/Skills.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import styles from '../assets/styles/Skills.module.css';
+
+interface SkillsProps {}
+
+const Skills: React.FC<SkillsProps> = ({}) => {
+  return <section id="skills" aria-label="Skills"></section>;
+};
+
+export default Skills;

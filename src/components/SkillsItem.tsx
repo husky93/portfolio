b5fb1fcc2fdf4fe017dc215e7b6eaf71d1ae4943
Maciej Sroka <sroka.maciej93@gmail.com>
2022-10-09 14:45:56 +0200

    Create Skills Item component template

---
+++ src/components/SkillsItem.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import styles from '../assets/styles/SkillsItem.module.css';
+
+interface SkillsItemProps {
+  children: React.ReactNode;
+  title: string;
+}
+
+const SkillsItem: React.FC<SkillsItemProps> = ({ title, children }) => {
+  return (
+    <li className={styles.item}>
+      <div className={styles.icon}>{children}</div>
+      <span className={styles.title}>Title</span>
+    </li>
+  );
+};
+
+export default SkillsItem;

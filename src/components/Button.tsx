77cf0c1b611d3926f5ae461fca24a91390c8c957
Maciej Sroka <sroka.maciej93@gmail.com>
2022-10-11 16:40:20 +0200

    Extend button functionality to include button type attribute

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -3,14 +3,19 @@ import styles from '../assets/styles/Button.module.css';
 
 interface ButtonProps {
   children: React.ReactNode;
-  navigateTo: string;
+  navigateTo?: string;
+  type?: 'button' | 'reset' | 'submit' | undefined;
 }
 
-const Button: React.FC<ButtonProps> = ({ children, navigateTo }) => {
-  return (
+const Button: React.FC<ButtonProps> = ({ children, navigateTo, type }) => {
+  return navigateTo ? (
     <a className={styles.button} href={navigateTo} target="_blank">
       {children}
     </a>
+  ) : (
+    <button className={styles.button} type={type}>
+      {children}
+    </button>
   );
 };
 

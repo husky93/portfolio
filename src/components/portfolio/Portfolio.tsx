1617360301f712daab552354a10a14cb9f838230
Maciej Sroka <sroka.maciej93@gmail.com>
2022-10-12 19:22:33 +0200

    Add Portfolio section content

--- src/components/portfolio/Portfolio.tsx
+++ src/components/portfolio/Portfolio.tsx
@@ -1,6 +1,17 @@
 import React, { lazy } from 'react';
 import styles from '../../assets/styles/portfolio/Portfolio.module.css';
 import { ReactComponent as ReactIcon } from '../../assets/icons/react.svg';
+import { ReactComponent as ViteIcon } from '../../assets/icons/vite.svg';
+import { ReactComponent as TsIcon } from '../../assets/icons/typescript.svg';
+import { ReactComponent as CssIcon } from '../../assets/icons/css.svg';
+import { ReactComponent as ReduxIcon } from '../../assets/icons/redux.svg';
+import { ReactComponent as TailwindIcon } from '../../assets/icons/tailwindcss.svg';
+import { ReactComponent as FirebaseIcon } from '../../assets/icons/firebase.svg';
+import { ReactComponent as JestIcon } from '../../assets/icons/jest.svg';
+import { ReactComponent as WebpackIcon } from '../../assets/icons/webpack.svg';
+import { ReactComponent as JsIcon } from '../../assets/icons/js.svg';
+import { ReactComponent as TestingIcon } from '../../assets/icons/testing-library.svg';
+import { ReactComponent as StyledIcon } from '../../assets/icons/styled.svg';
 import { ReactComponent as ArrowIcon } from '../../assets/icons/arrowupright.svg';
 
 interface PortfolioProps {}
@@ -16,61 +27,115 @@ const Portfolio: React.FC<PortfolioProps> = ({}) => {
         <h2 className="h2">My Projects</h2>
         <Project
           title="Portfolio"
-          description="..."
+          description="This is a website you're on right now! The goal was to make an interactive, accessible portfolio website with good performance. Something caught your attention? See how I programmed it by clicking Code button below!"
           imageName="placeholder"
           imagePosition="left"
           ghLink="https://github.com/husky93/portfolio"
         >
           <Badge>
+            <TsIcon /> TypeScript
+          </Badge>
+          <Badge>
             <ReactIcon /> React
+          </Badge>
+          <Badge>
+            <CssIcon /> CSS Modules
+          </Badge>
+          <Badge>
+            <ViteIcon /> Vite
           </Badge>
         </Project>
         <Project
           title="Social Blogging Platform"
-          description="..."
+          description="Social Blogging Platform based on websites like medium.com and dev.to. The goal was to program as much of core functionality of such platform in a limited time. You can login, add new posts, comment, bookmark and like them. For full list of features you can read project documentation by clicking Code button below."
           imageName="placeholder"
           imagePosition="right"
           ghLink="https://github.com/husky93/social-blogging-platform"
           liveLink="https://husky93.github.io/social-blogging-platform/"
         >
           <Badge>
+            <TsIcon /> TypeScript
+          </Badge>
+          <Badge>
             <ReactIcon /> React
+          </Badge>
+          <Badge>
+            <ReduxIcon /> Redux-Toolkit
+          </Badge>
+          <Badge>
+            <TailwindIcon /> TailwindCSS
+          </Badge>
+          <Badge>
+            <ViteIcon /> Vite
+          </Badge>
+          <Badge>
+            <FirebaseIcon /> Firebase
           </Badge>
         </Project>
         <Project
           title="Where's Waldo Game"
-          description="..."
+          description="Where's Waldo image tagging game coded with use of React library and Test-Driven-Development methodology. Find Waldo and his friends and save your score! The goal of this project was to learn more about TDD with React and find out more about frontend connection with backend"
           imageName="placeholder"
           imagePosition="left"
           ghLink="https://github.com/husky93/photo-tagging-app"
           liveLink="https://husky93.github.io/photo-tagging-app/"
         >
           <Badge>
+            <JsIcon /> JavaScript
+          </Badge>
+          <Badge>
             <ReactIcon /> React
+          </Badge>
+          <Badge>
+            <StyledIcon /> styled-components
+          </Badge>
+          <Badge>
+            <JestIcon /> jest
+          </Badge>
+          <Badge>
+            <TestingIcon /> testing-library
+          </Badge>
+          <Badge>
+            <FirebaseIcon /> Firebase
           </Badge>
         </Project>
         <Project
           title="Store Frontend"
-          description="..."
+          description="Interactive Frontend of a Store coded with use of React library and Test-Driven-Development methodology. Goal was to get used with routing in React and practice React components interactions as well as responsive styling."
           imageName="placeholder"
           imagePosition="right"
           ghLink="https://github.com/husky93/storefront"
           liveLink="https://husky93.github.io/storefront/"
         >
           <Badge>
+            <JsIcon /> JavaScript
+          </Badge>
+          <Badge>
             <ReactIcon /> React
+          </Badge>
+          <Badge>
+            <JestIcon /> jest
+          </Badge>
+          <Badge>
+            <TestingIcon /> testing-library
           </Badge>
         </Project>
         <Project
           title="Battleships Game"
-          description="..."
+          description="A Battleships game web application written fully in JavaScript with use of Test-Driven-Development methodology and OOP principles. Place your ships and play against an advanced AI that chooses which tile to hit based on already hit ship tiles. The goal of this project was to show my vanilla JS skills."
           imageName="placeholder"
           imagePosition="left"
           ghLink="https://github.com/husky93/battleship-game"
           liveLink="https://husky93.github.io/battleship-game/"
         >
           <Badge>
-            <ReactIcon /> React
+            <JsIcon /> Vanilla JavaScript
+          </Badge>
+          <Badge>
+            <WebpackIcon /> webpack
+          </Badge>
+          <Badge>
+            <JestIcon /> jest
           </Badge>
         </Project>
       </div>

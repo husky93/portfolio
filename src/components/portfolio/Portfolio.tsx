6805b2028cffd1748063f7087002403110fd74a5
Maciej Sroka <sroka.maciej93@gmail.com>
2022-10-12 17:42:25 +0200

    Align portfolio section

--- src/components/portfolio/Portfolio.tsx
+++ src/components/portfolio/Portfolio.tsx
@@ -1,75 +1,84 @@
 import React, { lazy } from 'react';
 import styles from '../../assets/styles/portfolio/Portfolio.module.css';
 import { ReactComponent as ReactIcon } from '../../assets/icons/react.svg';
+import { ReactComponent as ArrowIcon } from '../../assets/icons/arrowupright.svg';
 
 interface PortfolioProps {}
 
 const Project = lazy(() => import('./Project'));
 const Badge = lazy(() => import('../Badge'));
+const Button = lazy(() => import('../Button'));
 
 const Portfolio: React.FC<PortfolioProps> = ({}) => {
   return (
     <section className={styles.projects} id="projects">
-      <h2 className="h2">My Projects</h2>
-      <Project
-        title="Portfolio"
-        description="..."
-        imageName="placeholder"
-        imagePosition="left"
-        ghLink="https://github.com/husky93/portfolio"
-      >
-        <Badge>
-          <ReactIcon /> React
-        </Badge>
-      </Project>
-      <Project
-        title="Social Blogging Platform"
-        description="..."
-        imageName="placeholder"
-        imagePosition="right"
-        ghLink="https://github.com/husky93/social-blogging-platform"
-        liveLink="https://husky93.github.io/social-blogging-platform/"
-      >
-        <Badge>
-          <ReactIcon /> React
-        </Badge>
-      </Project>
-      <Project
-        title="Where's Waldo Game"
-        description="..."
-        imageName="placeholder"
-        imagePosition="left"
-        ghLink="https://github.com/husky93/photo-tagging-app"
-        liveLink="https://husky93.github.io/photo-tagging-app/"
-      >
-        <Badge>
-          <ReactIcon /> React
-        </Badge>
-      </Project>
-      <Project
-        title="Store Frontend"
-        description="..."
-        imageName="placeholder"
-        imagePosition="right"
-        ghLink="https://github.com/husky93/storefront"
-        liveLink="https://husky93.github.io/storefront/"
-      >
-        <Badge>
-          <ReactIcon /> React
-        </Badge>
-      </Project>
-      <Project
-        title="Battleships Game"
-        description="..."
-        imageName="placeholder"
-        imagePosition="left"
-        ghLink="https://github.com/husky93/battleship-game"
-        liveLink="https://husky93.github.io/battleship-game/"
-      >
-        <Badge>
-          <ReactIcon /> React
-        </Badge>
-      </Project>
+      <div className={styles.content}>
+        <h2 className="h2">My Projects</h2>
+        <Project
+          title="Portfolio"
+          description="..."
+          imageName="placeholder"
+          imagePosition="left"
+          ghLink="https://github.com/husky93/portfolio"
+        >
+          <Badge>
+            <ReactIcon /> React
+          </Badge>
+        </Project>
+        <Project
+          title="Social Blogging Platform"
+          description="..."
+          imageName="placeholder"
+          imagePosition="right"
+          ghLink="https://github.com/husky93/social-blogging-platform"
+          liveLink="https://husky93.github.io/social-blogging-platform/"
+        >
+          <Badge>
+            <ReactIcon /> React
+          </Badge>
+        </Project>
+        <Project
+          title="Where's Waldo Game"
+          description="..."
+          imageName="placeholder"
+          imagePosition="left"
+          ghLink="https://github.com/husky93/photo-tagging-app"
+          liveLink="https://husky93.github.io/photo-tagging-app/"
+        >
+          <Badge>
+            <ReactIcon /> React
+          </Badge>
+        </Project>
+        <Project
+          title="Store Frontend"
+          description="..."
+          imageName="placeholder"
+          imagePosition="right"
+          ghLink="https://github.com/husky93/storefront"
+          liveLink="https://husky93.github.io/storefront/"
+        >
+          <Badge>
+            <ReactIcon /> React
+          </Badge>
+        </Project>
+        <Project
+          title="Battleships Game"
+          description="..."
+          imageName="placeholder"
+          imagePosition="left"
+          ghLink="https://github.com/husky93/battleship-game"
+          liveLink="https://husky93.github.io/battleship-game/"
+        >
+          <Badge>
+            <ReactIcon /> React
+          </Badge>
+        </Project>
+      </div>
+      <div className={styles.viewmore}>
+        <Button navigateTo="https://github.com/husky93?tab=repositories">
+          View More <ArrowIcon />
+        </Button>
+      </div>
     </section>
   );
 };

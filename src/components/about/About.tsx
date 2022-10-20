59aac87a8708eaa82bd0572ff77a9c9d025e49da
Maciej Sroka <sroka.maciej93@gmail.com>
2022-10-20 18:51:53 +0200

    Minor changes

--- src/components/about/About.tsx
+++ src/components/about/About.tsx
@@ -65,42 +65,37 @@ const About: React.FC<AboutProps> = ({}) => {
                     {' '}
                     World Wide Web{' '}
                   </strong>
-                  and its potential. I had an internet access since it started
-                  gaining popularity in Poland (around 2001). I witnessed it
-                  transform from websites with bunch of links and text and
-                  barely any style at all to global, fast, functional, modern
-                  social media platforms we have today.
-                </p>
-                <p className={styles.desc}>
-                  My interest in{' '}
+                  and its potential. My interest in{' '}
                   <strong className={styles.highlighted}>
                     {' '}
                     web development{' '}
                   </strong>
                   started when i was around 13. Thats when I created my first
                   website based on jPortal2 CMS. I didn't follow up on my
-                  passion back then, as it seemed very hard to do things on a
-                  web for a 13 y.o. without communities like StackOverflow and
+                  interest back then, as it seemed very hard to build things on
+                  a web for a 13 y.o. without communities like StackOverflow and
                   decent Google SE algorithm 😆.
                 </p>
+
                 <p className={styles.desc}>
                   After many years I decided to revisit old passion of mine and
                   learn{' '}
                   <strong className={styles.highlighted}>
-                    computer science and programming
+                    computer science and programming.
                   </strong>{' '}
-                  through the internet. I got instantly hooked in and over the
-                  last few years I have gained solid knowledge of{' '}
+                  I have instantly felt like fish in the water, and got hooked
+                  into the world of coding. Over the last few years I have
+                  gained solid knowledge of{' '}
                   <strong className={styles.highlighted}>
                     frontend development
                   </strong>{' '}
-                  and programming in general. Although there is still a lot to
-                  learn I believe I can be a valuable asset to a Frontend
+                  and programming in general. Although there is still a lot for
+                  me to learn I believe I can be a valuable asset to a Frontend
                   Development team and continue my growth with a company that is
                   willing to help me with it. So if you're looking for a
                   motivated, willing-to-learn, developer with a solid knowledge
-                  and passion to build fast, accessible, responsive UIs - hit me
-                  up 😁!
+                  and passion to build fast, functional, accessible, responsive
+                  UIs - hit me up 😁!
                 </p>
               </div>
             </Curtain>

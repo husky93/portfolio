3109201454394a7811d1f364903b0196c7515e36
Maciej Sroka <sroka.maciej93@gmail.com>
2022-12-11 12:19:27 +0100

    Refactor Hero section alignment and responsiveness

--- src/components/hero/Hero.tsx
+++ src/components/hero/Hero.tsx
@@ -1,7 +1,7 @@
 import React, { lazy, useState, useEffect } from 'react';
 import styles from '../../assets/styles/hero/Hero.module.css';
 
-const HeroBackground = lazy(() => import('./HeroBackground'));
+const Sphere = lazy(() => import('./Sphere'));
 const AnimatedHeading = lazy(() => import('./AnimatedHeading'));
 const AlternatingText = lazy(() => import('./AlternatingText'));
 
@@ -16,23 +16,27 @@ const Hero: React.FC<HeroProps> = () => {
 
   return (
     <section className={styles.hero} id="hero">
-      <HeroBackground />
-      <div className={styles.content}>
-        <p className={`${styles.introduction} ${loaded ? styles.loaded : ''}`}>
-          Hi, my name is
-        </p>
-        <h1 className={styles.header}>
-          <AnimatedHeading text="Maciej" active />
-          <AnimatedHeading text="Sroka" active />
-        </h1>
-        <AlternatingText
-          text={[
-            'JavaScript Developer',
-            'TypeScript',
-            'React',
-            'Node.js + Express',
-          ]}
-        />
+      <div className={styles.wrapper}>
+        <div className={styles.content}>
+          <p
+            className={`${styles.introduction} ${loaded ? styles.loaded : ''}`}
+          >
+            Hi, my name is
+          </p>
+          <h1 className={styles.header}>
+            <AnimatedHeading text="Maciej" active />
+            <AnimatedHeading text="Sroka" active />
+          </h1>
+          <AlternatingText
+            text={[
+              'JavaScript Developer',
+              'TypeScript',
+              'React',
+              'Node.js + Express',
+            ]}
+          />
+        </div>
+        <Sphere />
       </div>
     </section>
   );
